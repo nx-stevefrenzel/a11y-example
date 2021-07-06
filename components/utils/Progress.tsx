609772f293411290e6a141d5e3c5c00edee45cb8
Steve Frenzel <69304709+stvfrnzlnxm@users.noreply.github.com>
2021-07-06 10:31:06 +0200

    Update Progress.tsx

--- components/utils/Progress.tsx
+++ components/utils/Progress.tsx
@@ -1,16 +1,15 @@
 import React, { ReactElement } from 'react';
 
 interface Props {
-  id: string;
   value: string;
   content: string;
 }
 
-export default function Progress({ id, content, value }: Props): ReactElement {
+export default function Progress({ content, value }: Props): ReactElement {
   return (
     <div className="progress-bar">
-      <label htmlFor={id}>{content}</label>
-      <progress id={id} max="100" value={value} aria-hidden="true" />
+      <span>{content}</span>
+      <progress max="100" value={value} aria-hidden="true" />
     </div>
   );
 }
