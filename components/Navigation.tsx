412270dad7ad6a61ce1f574ede47658a3b34e96c
Steve Frenzel <69304709+stvfrnzlnxm@users.noreply.github.com>
2021-07-08 14:40:30 +0200

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,25 +1,37 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function Navigation() {
+  const router = useRouter();
+  function addClass(path: string) {
+    if (router.pathname === path) {
+      return 'active';
+    } else {
+      return undefined;
+    }
+  }
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/" scroll={false}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/bad-example" scroll={false}>
-            Bad example
-          </Link>
-        </li>
-        <li>
-          <Link href="/good-example" scroll={false}>
-            Good example
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ol>
+          <li>
+            <Link href="/" scroll={false} passHref>
+              <a className={addClass('/')}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/bad-example" scroll={false}>
+              <a className={addClass('/bad-example')}>Bad example</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/good-example" scroll={false}>
+              <a className={addClass('/good-example')}>Good example</a>
+            </Link>
+          </li>
+        </ol>
+      </nav>
+    </header>
   );
 }
